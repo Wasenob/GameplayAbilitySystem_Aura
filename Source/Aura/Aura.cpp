0a4a6d2785d6f9debe0717b7f09ab9b8ff1f08b4
Wasenob <yourmomisaboot@yahoo.com>
2024-08-28 18:45:44 -0500

    The Base Character Class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Mark McCarthy
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Mark McCarthy
+
+#pragma once
+
+#include "CoreMinimal.h"
+
