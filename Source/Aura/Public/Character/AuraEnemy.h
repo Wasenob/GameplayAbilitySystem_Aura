8e8f5ee5fba92049e3e0781895ab64209d160dc5
Wasenob <yourmomisaboot@yahoo.com>
2024-10-04 01:59:11 -0500

    Init Ability Actor Info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,9 +16,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
+
+	/** Enemy Interface */
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
+	/** end Enemy Interface */
 
-	UPROPERTY(BlueprintReadWrite)
-	bool bHighlighted = false;
+protected:
+	virtual void BeginPlay() override;
 };
