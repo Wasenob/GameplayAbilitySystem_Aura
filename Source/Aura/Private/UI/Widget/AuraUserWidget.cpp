116169b1537a68cbde6f3756e1798ecc21f28dc2
Wasenob <yourmomisaboot@yahoo.com>
2024-10-09 22:27:04 -0500

    Globe Progress Bar

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Mark McCarthy
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
