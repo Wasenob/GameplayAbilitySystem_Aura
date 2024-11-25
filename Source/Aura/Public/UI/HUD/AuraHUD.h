cfbdc8394f2fe0151d79fffa7ea6519b8826b76f
Wasenob <yourmomisaboot@yahoo.com>
2024-11-24 21:33:38 -0600

    Attribute Info Delegate

--- Source/Aura/Public/UI/HUD/AuraHUD.h
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -6,25 +6,25 @@
 #include "GameFramework/HUD.h"
 #include "AuraHUD.generated.h"
 
+class UAttributeMenuWidgetController;
+class UAttributeSet;
+class UAbilitySystemComponent;
 class UOverlayWidgetController;
 class UAuraUserWidget;
-class UAbilitySystemComponent;
-class UAttributeSet;
-class APlayerState;
 struct FWidgetControllerParams;
 
 /**
  * 
  */
+
 UCLASS()
 class AURA_API AAuraHUD : public AHUD
 {
 	GENERATED_BODY()
 public:
-	UPROPERTY()
-	TObjectPtr<UAuraUserWidget> OverlayWidget;
 
 	UOverlayWidgetController* GetOverlayWidgetController(const FWidgetControllerParams& WCParams);
+	UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const FWidgetControllerParams& WCParams);
 
 	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
 
@@ -32,6 +32,9 @@ protected:
 
 
 private:
+
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget>  OverlayWidget;	
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
@@ -41,4 +44,10 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	TSubclassOf<UOverlayWidgetController> OverlayWidgetControllerClass;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeMenuWidgetController> AttributeMenuWidgetController;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAttributeMenuWidgetController> AttributeMenuWidgetControllerClass;
 };
